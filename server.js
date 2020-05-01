75871aec4d5f9df881ad90986a304ef4e0479652
ahonore42 <a1honore80@gmail.com>
2020-05-01 15:31:09 -0700

    App complete for requirements

--- server.js
+++ server.js
@@ -7,10 +7,41 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('static'));
 app.use(methodOverride('_method'));
+let db = require('./models')
 
 // WRITE YOUR ROUTES HERE /////////////////////
 
+app.get('/', (req, res) => {
+    db.widget.findAll()
+    .then((widgets) => {
+        res.render('index', {widgets} )
+    })
+})
 
+app.post('/', (req, res) => {
+    db.widget.create({
+      description: req.body.description,
+      quantity: req.body.quantity,
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch((err) => {
+      console.log('ERROR! DOES NOT COMPUTE!', err)
+    })
+  })
+
+  app.delete('/:id', (req, res) => {
+    db.widget.destroy({
+      where: { id: req.params.id }
+    })
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch(err => {
+      console.log('DELETE THE HUMAN, BRRRRRRR', err)
+    })
+  })
 
 // YOUR ROUTES ABOVE THIS COMMENT /////////////
 
